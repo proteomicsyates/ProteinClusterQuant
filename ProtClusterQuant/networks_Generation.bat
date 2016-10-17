50ccad5ff137255004e1f1fc5151a8b30ac9cffa
Salvador Martinez-Bartolome <salvador@scripps.edu>
2016-10-17 15:06:52 -0700

    change in maven-compiler-plugin version

--- ProtClusterQuant/networks_Generation.bat
+++ ProtClusterQuant/networks_Generation.bat
@@ -5,14 +5,14 @@ REM java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSH
 java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f "C:\Users\Salva\Desktop\data\PINT projects\Fibulin\Fibulin_RW_NEW_DB_3PSMsPerPepNode.properties"
 java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f "C:\Users\Salva\Desktop\data\PINT projects\Fibulin\Fibulin_lacZ_NEW_DB_3PSMsPerPepNode.properties"
 java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f "C:\Users\Salva\Desktop\data\PINT projects\Fibulin\Fibulin_WT_NEW_DB_3PSMsPerPepNode.properties"
-java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_noFilter_Rc_IDs.properties
-java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_1ionsPerNode_Rc_IDs.properties
-java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_2ionsPerNode_Rc_IDs.properties
-java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_3ionsPerNode_Rc_IDs.properties
-java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_3ionsPerNode_Rc_IDs.properties
-java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_4ionsPerNode_Rc_IDs.properties
-java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_5ionsPerNode_Rc_IDs.properties
-java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_6ionsPerNode_Rc_IDs.properties
-java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_7ionsPerNode_Rc_IDs.properties
-java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_8ionsPerNode_Rc_IDs.properties
-java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_9ionsPerNode_Rc_IDs.properties
+REM java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_noFilter_Rc_IDs.properties
+REM java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_1ionsPerNode_Rc_IDs.properties
+REM java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_2ionsPerNode_Rc_IDs.properties
+REM java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_3ionsPerNode_Rc_IDs.properties
+REM java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_3ionsPerNode_Rc_IDs.properties
+REM java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_4ionsPerNode_Rc_IDs.properties
+REM java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_5ionsPerNode_Rc_IDs.properties
+REM java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_6ionsPerNode_Rc_IDs.properties
+REM java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_7ionsPerNode_Rc_IDs.properties
+REM java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_8ionsPerNode_Rc_IDs.properties
+REM java -jar C:\Users\Salva\Desktop\Dropbox\tmp\PCQ\ProtClusterQuant-1.3-SNAPSHOT.jar -f setup_CFBEvsHBE_protpepCollapsed_9ionsPerNode_Rc_IDs.properties
